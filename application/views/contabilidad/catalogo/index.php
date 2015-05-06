d0358895e795dfc9a021a9818e117631ae1e5919
phurtado1112 <phurtado1112@gmail.com>
2015-05-05 22:47:31 -0600

    Merge branch 'master' of https://github.com/phurtado1112/cuentas
    Conflicts:
    	catalogos/MetodosCatalogos.php
    	catalogos/categoriascuentas/CategoriasCuentas.php
    	catalogos/categoriascuentas/categorias_cuentas_crear.php
    	catalogos/categoriascuentas/categorias_cuentas_procesar.php

--- application/views/contabilidad/catalogo/index.php
+++ application/views/contabilidad/catalogo/index.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Principal</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/bootstrap.min.css">
     </head>
     <body>
         
     </body>
-    <script src="<?php echo base_url();?>bootstrap/js/jquery-2.1.3.min.js"></script>
-    <script src="<?php echo base_url();?>bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>public/js/jquery-2.1.3.min.js"></script>
+    <script src="<?php echo base_url();?>public/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>public/js/jquery-select.js"></script>
 </html>
--- application/views/contabilidad/index.php
+++ application/views/contabilidad/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Principal</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/bootstrap.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>public/font-awesome-4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/estilo.css">
         <style type="text/css">
@@ -18,8 +18,8 @@
     <body>
         <img class="grande img-responsive" src="<?php echo base_url(); ?>public/img/cacao.png">
         <img class="mediana img-responsive" src="<?php echo base_url(); ?>public/img/contabilidad.png">
-        <script src="<?php echo base_url(); ?>bootstrap/js/jquery-2.1.3.min.js"></script>
-        <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/js/jquery-2.1.3.min.js"></script>
+        <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url(); ?>public/js/jquery-select.js"></script>
     </body>
 
